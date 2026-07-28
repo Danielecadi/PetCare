afa1db9845ee9ada5bea080c4f67a7c0b1ed4f93
Daniele Cadicina <xwsdtk9hcg@privaterelay.appleid.com>
2026-07-28 16:02:10 +0200

    Traduzione interfaccia in italiano

--- database/seeders/SpeciesSeeder.php
+++ database/seeders/SpeciesSeeder.php
@@ -13,10 +13,38 @@ class SpeciesSeeder extends Seeder
      */
     public function run(): void
     {
-        $species = ['Cat', 'Dog', 'Bird', 'Rabbit', 'Fish', 'Reptile', 'Horse', 'Cow', 'Sheep', 'Goat', 'Pig', 'Chicken', 'Duck', 'Turkey', 'Guinea Pig', 'Hamster', 'Ferret', 'Chinchilla', 'Parrot', 'Turtle'];
+        $speciesMap = [
+            'Cat' => 'Gatto',
+            'Dog' => 'Cane',
+            'Bird' => 'Uccello',
+            'Rabbit' => 'Coniglio',
+            'Fish' => 'Pesce',
+            'Reptile' => 'Rettile',
+            'Horse' => 'Cavallo',
+            'Cow' => 'Mucca',
+            'Sheep' => 'Pecora',
+            'Goat' => 'Capra',
+            'Pig' => 'Maiale',
+            'Chicken' => 'Pollo',
+            'Duck' => 'Anatra',
+            'Turkey' => 'Tacchino',
+            'Guinea Pig' => 'Porcellino d\'India',
+            'Hamster' => 'Criceto',
+            'Ferret' => 'Furetto',
+            'Chinchilla' => 'Cincillà',
+            'Parrot' => 'Pappagallo',
+            'Turtle' => 'Tartaruga',
+        ];
 
-        foreach ($species as $specie) {
-            Species::firstOrCreate(['name' => $specie]);
+        foreach ($speciesMap as $legacyName => $italianName) {
+            $species = Species::where('name', $legacyName)->first();
+
+            if ($species) {
+                $species->update(['name' => $italianName]);
+                continue;
+            }
+
+            Species::firstOrCreate(['name' => $italianName]);
         }
     }
 }
